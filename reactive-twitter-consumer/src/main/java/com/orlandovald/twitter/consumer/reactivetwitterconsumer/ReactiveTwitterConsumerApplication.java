0b51ee48fbc73482a288152d3e1ecc48b9257ed6
Orlando Valdez <orlandovald@gmail.com>
2018-10-19 19:58:42 -0400

    Initial commit

---
+++ reactive-twitter-consumer/src/main/java/com/orlandovald/twitter/consumer/reactivetwitterconsumer/ReactiveTwitterConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.orlandovald.twitter.consumer.reactivetwitterconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTwitterConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveTwitterConsumerApplication.class, args);
+	}
+}
