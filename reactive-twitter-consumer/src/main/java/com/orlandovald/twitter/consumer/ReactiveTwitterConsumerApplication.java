c71b54637881fc7f6b31d430c0149061eb742cd2
Orlando Valdez <orlandovald@gmail.com>
2018-10-20 21:05:56 -0400

    Remove unnecessary package

--- reactive-twitter-consumer/src/main/java/com/orlandovald/twitter/consumer/ReactiveTwitterConsumerApplication.java
+++ reactive-twitter-consumer/src/main/java/com/orlandovald/twitter/consumer/ReactiveTwitterConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.orlandovald.twitter.consumer.reactivetwitterconsumer;
+package com.orlandovald.twitter.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
