3b501966dc4ad4c0d8f47f936e65b7f0c3a095de
Orlando Valdez <orlandovald@gmail.com>
2018-10-24 20:35:34 -0400

    Tracks as program arguments refactoring and clean up

--- reactive-twitter-consumer/src/main/java/com/orlandovald/twitter/consumer/ReactiveTwitterConsumerApplication.java
+++ reactive-twitter-consumer/src/main/java/com/orlandovald/twitter/consumer/ReactiveTwitterConsumerApplication.java
@@ -2,77 +2,75 @@ package com.orlandovald.twitter.consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.Assert;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import static java.util.stream.Collectors.joining;
+
 @EnableConfigurationProperties(TwitterConsumerProperties.class)
 @SpringBootApplication
 public class ReactiveTwitterConsumerApplication {
 
-	public static Logger log = LoggerFactory.getLogger(ReactiveTwitterConsumerApplication.class);
+    public static Logger log = LoggerFactory.getLogger(ReactiveTwitterConsumerApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactiveTwitterConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveTwitterConsumerApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner twitterStream(WebClient.Builder wcb, OAuth1SignatureUtil oauthUtil) {
-		return args -> {
-			WebClient webClient = wcb
-					.baseUrl("https://stream.twitter.com/1.1")
-					.filter(oauthFilter(oauthUtil))
-					.filter(logRequest())
-					.filter(logResponse())
-					.build();
-			Flux<String> tweets = webClient.get().uri(uriBuilder -> uriBuilder.path("/statuses/filter.json")
-					.queryParam("track", "realDonaldTrump")
-					.build())
-					.exchange().flatMapMany(clientResponse -> clientResponse.bodyToFlux(String.class));
-//                    .retrieve().bodyToFlux(String.class);
-			tweets.subscribe(System.out::println);
+    @Bean
+    ApplicationRunner twitterStream(WebClient.Builder wcb, OAuth1SignatureUtil oauthUtil) {
+        return args -> {
+            Assert.isTrue(args.containsOption("track"), "[--track] argument is required");
+            String tracks = args.getOptionValues("track").stream().collect(joining(","));
+            Assert.hasText(tracks, "[--track] argument must contain at least one value");
 
-		};
-	}
+            WebClient webClient = wcb
+                    .baseUrl("https://stream.twitter.com/1.1")
+                    .filter(oauthFilter(oauthUtil))
+                    .filter(logRequest())
+                    .build();
 
-	@Bean
-	OAuth1SignatureUtil oAuth1SignatureUtil(TwitterConsumerProperties props) {
-		OAuth1Credentials creds = new OAuth1Credentials(props.getApi().getKey(), props.getApi().getSecret(),
-				props.getApi().getAccessToken(), props.getApi().getAccessTokenSecret());
-		return new OAuth1SignatureUtil(creds);
-	}
+            Flux<String> tweets = webClient.get().uri(uriBuilder -> uriBuilder.path("/statuses/filter.json")
+                    .queryParam("track", tracks)
+                    .build())
+                    .exchange().flatMapMany(clientResponse -> clientResponse.bodyToFlux(String.class));
 
-	private static ExchangeFilterFunction oauthFilter(OAuth1SignatureUtil oauthUtil) {
-		return ExchangeFilterFunction.ofRequestProcessor(req -> {
-			ClientRequest oauthReq = ClientRequest.from(req)
-					.headers(headers -> headers.add(HttpHeaders.AUTHORIZATION, oauthUtil.oAuth1Header(req)))
-					.build();
-			return Mono.just(oauthReq);
-		});
-	}
+            tweets.subscribe(System.out::println);
+        };
+    }
 
-	private static ExchangeFilterFunction logRequest() {
-		return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
-			log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
-			clientRequest.headers().forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
-			return Mono.just(clientRequest);
-		});
-	}
+    @Bean
+    OAuth1SignatureUtil oAuth1SignatureUtil(TwitterConsumerProperties props) {
+        OAuth1Credentials creds = new OAuth1Credentials(props.getApi().getKey(), props.getApi().getSecret(),
+                props.getApi().getAccessToken(), props.getApi().getAccessTokenSecret());
+        return new OAuth1SignatureUtil(creds);
+    }
 
-	private static ExchangeFilterFunction logResponse() {
-		return ExchangeFilterFunction.ofResponseProcessor(response -> {
-			log.info("Response code: {}",response.statusCode().toString());
-			response.headers().asHttpHeaders().forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
-			return Mono.just(response);
-		});
-	}
+    private ExchangeFilterFunction oauthFilter(OAuth1SignatureUtil oauthUtil) {
+        return ExchangeFilterFunction.ofRequestProcessor(req -> {
+            ClientRequest oauthReq = ClientRequest.from(req)
+                    .headers(headers -> headers.add(HttpHeaders.AUTHORIZATION, oauthUtil.oAuth1Header(req)))
+                    .build();
+            return Mono.just(oauthReq);
+        });
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(req -> {
+            log.info("Request: {} {}", req.method(), req.url());
+            req.headers().forEach((name, values) -> values.forEach(value -> log.info("Header: {}={}", name, value)));
+            return Mono.just(req);
+        });
+    }
 
 }
