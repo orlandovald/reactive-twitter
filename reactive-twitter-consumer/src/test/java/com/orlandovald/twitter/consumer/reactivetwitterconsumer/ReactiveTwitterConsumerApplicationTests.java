d0713fc75fc198e0dec5e747e46efbe1a30f00c5
Orlando Valdez <orlandovald@gmail.com>
2018-12-15 19:26:22 -0500

    Disable applicationRunner in test phase

--- reactive-twitter-consumer/src/test/java/com/orlandovald/twitter/consumer/reactivetwitterconsumer/ReactiveTwitterConsumerApplicationTests.java
+++ reactive-twitter-consumer/src/test/java/com/orlandovald/twitter/consumer/reactivetwitterconsumer/ReactiveTwitterConsumerApplicationTests.java
@@ -1,12 +1,15 @@
 package com.orlandovald.twitter.consumer.reactivetwitterconsumer;
 
+import com.orlandovald.twitter.consumer.ReactiveTwitterConsumerApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ContextConfiguration(classes = ReactiveTwitterConsumerApplication.class,
+		initializers = ConfigFileApplicationContextInitializer.class)
 public class ReactiveTwitterConsumerApplicationTests {
 
 	@Test
